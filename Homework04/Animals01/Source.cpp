70e4835d804ef7dc845ce3c5818ec8f62ebbe855
JoshuaJRMorris <joshuajrmorris@gmail.com>
2020-02-06 21:14:53 -0400

    Homework04

---
+++ Homework04/Animals01/Source.cpp
@@ -0,0 +1,72 @@
+/*
+* @file <filename>
+*
+* @author  <Joshua Morris joshuajrmorris@gmail.com>
+* @version <1.0>
+*
+* change log
+* name   Josh Morris   date 2020-02-06
+*
+*
+* @section Academic Integrity
+*  I certify that this work is solely my own and complies with
+*  NBCC Academic Integrity Policy (policy 1111)
+*
+*
+* @section DESCRIPTION
+*  < inheritance demo >
+*
+*
+* @section LICENSE
+*  <any necessary attributions>
+*
+*  Copyright 2020
+*  Permission to use, copy, modify, and/or distribute this software for
+*  any purpose with or without fee is hereby granted, provided that the
+*  above copyright notice and this permission notice appear in all copies.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+*  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+*  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+*  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+*  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+*  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+*  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*
+*/
+//
+//
+//
+
+#include "Animal.h"
+#include "Cat.h"
+#include "Dog.h"
+#include "Duck.h"
+#include "Fox.h"
+#include <iostream>
+#include <ctime>
+
+
+int main() {
+
+
+	
+
+
+
+
+
+		Cat cat("Sabrina");
+		Dog dog("Fido");
+		Duck duck("Donald");
+		Fox fox("Ylvis");
+
+
+		cat.speak();
+		dog.speak();
+		duck.speak();
+		fox.speak();
+
+
+	
+}
---
+++ Homework04/Animals01/test.cpp
@@ -0,0 +1,26 @@
+//#include "gtest/gtest.h"
+//#include "Animal.h"
+//#include "Cat.h"
+//#include "Dog.h"
+//#include "Duck.h"
+//#include "Fox.h"
+//#include <iostream>
+//
+//
+//
+//
+//
+//TEST(TestCaseName, TestName) {
+//	Cat cat("Sabrina");
+//	Dog dog("Fido");
+//	Duck duck("Donald");
+//	Fox fox("Ylvis");
+//
+//
+//	cat.speak();
+//	dog.speak();
+//	duck.speak();
+//	fox.speak();
+//
+//	
+//}
